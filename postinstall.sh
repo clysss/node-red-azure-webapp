6d4e6b78fbbd76972bff957faa9736e07a15ba98
clysss <55925546+clysss@users.noreply.github.com>
2020-11-29 17:17:44 +0100

    Delete postinstall.sh

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-su
-mkdir /home/nodered
-chmod 777 /home/nodered
-mkdir /home/nodered/.node-red
-chmod 777 /home/nodered/.node-red
-cd /home/nodered/.node-red
-npm install node-red-dashboard
