cb57b3b760cf9302ad7d50d61bb977cb513c0459
clysss <55925546+clysss@users.noreply.github.com>
2020-11-29 19:17:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -189,11 +189,6 @@ module.exports = {
             // Whether or not to include audit events in the log output
             audit: false
         }
-    },
-    editorTheme: {
-      projects:{
-        enabled: true
-      }
     }
     
 };
